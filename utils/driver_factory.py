7f58a599780cf9e6a481d9592a70d7b527ec3785
DEVENDRA VISWASH <143888204+Devendra8003@users.noreply.github.com>
2026-08-01 00:45:29 +0530

    Update driver_factory.py

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import os
 
 
 def get_driver():
     options = Options()
     options.add_argument("--start-maximized")
+
+    if os.getenv("CI"):
+        options.add_argument("--headless=new")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
